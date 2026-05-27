42c1471f317b5d7df0d98747b83d9592e62a6eaf
SileLusar <silelusar@gmail.com>
2026-05-27 00:25:22 +0000

    Agregar archivos base y grafo complejo taller 7

---
+++ ClassTests.java
@@ -0,0 +1,29 @@
+public class ClassTests {
+    public static void main(String[] args) throws InterruptedException {
+        Graph graph = new Graph();
+
+        graph.addEdge(0, 1, 2);
+        graph.addEdge(0, 2, 4);
+        graph.addEdge(0, 3, 7);
+        graph.addEdge(1, 4, 3);
+        graph.addEdge(1, 5, 6);
+        graph.addEdge(2, 4, 1);
+        graph.addEdge(2, 5, 5);
+        graph.addEdge(3, 5, 2);
+        graph.addEdge(3, 6, 4);
+        graph.addEdge(4, 6, 5);
+        graph.addEdge(5, 6, 1);
+
+        SequentialShortestPathSolver sequential = new SequentialShortestPathSolver(graph, 6);
+        PathResult sequentialResult = sequential.solve(0);
+
+        System.out.println("Resultado secuencial:");
+        System.out.println(sequentialResult);
+
+        ParallelShortestPathSolver parallel = new ParallelShortestPathSolver(graph, 6);
+        PathResult parallelResult = parallel.solve(0);
+
+        System.out.println("Resultado paralelo:");
+        System.out.println(parallelResult);
+    }
+}
